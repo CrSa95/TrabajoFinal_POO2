50a3f542301c26727953174007afff1f5807d968
Nehuen Gallitelli <nehuengallijans@gmail.com>
2024-11-09 00:12:15 -0300

    Implementacion del Observer

--- src/trabajoFinal/SitioWeb/Suscriptor.java
+++ src/trabajoFinal/SitioWeb/Suscriptor.java
@@ -2,4 +2,7 @@ package trabajoFinal.SitioWeb;
 
 public interface Suscriptor {
 
+	public abstract void bajaDePrecio(Inmueble inmueble);
+	public abstract void cancelacionDeReserva(Inmueble inmueble);
+	public abstract void altaDeReserva();
 }
